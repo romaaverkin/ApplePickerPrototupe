b3ea8d221fe8c6e3b1b8173fca02f3a10e46b1ea
Roman <romaaverkin@gmail.com>
2021-04-26 09:42:07 +0700

    Программирование прототипа Apple Picker

---
+++ Assets/AppleTree.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Unspector")]
+    // Шаблон для создания яблок
+    public GameObject applePrefab;
+
+    // Скорость движения яблони
+    public float speed = 1f;
+
+    // Расстояние, на котором должно измениться направление движения яблони
+    public float ledtAndRightEdge = 10f;
+
+    // Вероятность случайного изменения направления движения
+    public float chanceToChangeDirections = 0.1f;
+
+    // Частота создания экземпляров яблок
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Сбрасывать яблоки паз в секунду        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Простое перемещение
+        // Изменение направления
+    }
+}
