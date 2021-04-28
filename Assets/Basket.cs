f2bbb058bfb605a7fd6b72ee29817ad7a8f81ef8
Roman <romaaverkin@gmail.com>
2021-04-28 15:29:45 +0700

    Добавление очков за пойманное яблоко

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        // Получить ссылку на игровой объект ScoreCounter
+        GameObject scoreGo = GameObject.Find("ScoreCounter");
+        // Получить компонент Text этого игрового объекта
+        scoreGT = scoreGo.GetComponent<Text>();
+        // Установить начальноге значение
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -35,6 +44,12 @@ public class Basket : MonoBehaviour
         if (collidedWidth.tag == "Apple")
         {
             Destroy(collidedWidth);
+
+            // Преобразовать текст ScoreGT в целое число
+            int score = int.Parse(scoreGT.text);
+            // Добавить очки за пойманное яблоко
+            score += 100;
+            scoreGT.text = score.ToString();
         }
     }
 }
