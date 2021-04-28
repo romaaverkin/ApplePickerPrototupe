b31d84bce9987d4e83668d0f25385a9d62c9f137
Roman <romaaverkin@gmail.com>
2021-04-28 16:41:17 +0700

    Уничтожение корзины после потери яблока

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -10,16 +10,19 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14f;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>();
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGo = Instantiate(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGo);
         }
     }
 
@@ -30,6 +33,15 @@ public class ApplePicker : MonoBehaviour
         {
             Destroy(tGO);
         }
+
+        // Удалить одну корзину
+        // Получить индекс последней корзины в basketList
+        int basketIndex = basketList.Count - 1;
+        // Получить ссылку на этот игровой объект Basket
+        GameObject tBasketGO = basketList[basketIndex];
+        // Исключить корзину из списка и удалить сам игровой объект
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
     }
 
     // Update is called once per frame
